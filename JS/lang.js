8fa62ca17254ecea3ea10c63e63dfc13d33040c3
Pedro Emilio Martinelli <80924116+PedroEmilioMartinelli@users.noreply.github.com>
2025-10-02 13:46:09 +0000

    atualizado

--- JS/lang.js
+++ JS/lang.js
@@ -9,8 +9,7 @@ const translations = {
       partners: "Parceiros",
     },
     title: "Tecnologia que Protege seu Grão",
-    subtitle:
-      "Preserve a qualidade e o valor da sua safra com a nossa tecnologia",
+    subtitle: "Preserve a qualidade e o valor da sua safra com a nossa tecnologia",
     button: "Conheça nossos produtos",
     footer1: "Todos os direitos reservados.",
     footer2: "Desenvolvido por Pedro Emilio Martinelli",
@@ -31,8 +30,7 @@ const translations = {
       partners: "Partners",
     },
     title: "Technology that Protects Your Grain",
-    subtitle:
-      "Keep your harvest’s quality and value with our advanced technology",
+    subtitle: "Keep your harvest’s quality and value with our advanced technology",
     button: "Explore our products",
     footer1: "All rights reserved.",
     footer2: "Developed by Pedro Emilio Martinelli",
@@ -48,8 +46,7 @@ const translations = {
       partners: "Socios",
     },
     title: "Tecnología que Protege su Grano",
-    subtitle:
-      "Mantenga la calidad y el valor de su cosecha con nuestra tecnología avanzada",
+    subtitle: "Mantenga la calidad y el valor de su cosecha con nuestra tecnología avanzada",
     button: "Conozca nuestros productos",
     footer1: "Todos los derechos reservados.",
     footer2: "Desarrollado por Pedro Emilio Martinelli",
@@ -69,24 +66,20 @@ switcher.addEventListener("change", () => {
 
   // Header
   document.querySelector(".nav-home").textContent = translations[lang].nav.home;
-  document.querySelector(".nav-company").textContent =
-    translations[lang].nav.company;
-  document.querySelector(".nav-training").textContent =
-    translations[lang].nav.training;
-  document.querySelector(".nav-videos").textContent =
-    translations[lang].nav.videos;
-  document.querySelector(".nav-events").textContent =
-    translations[lang].nav.events;
-  document.querySelector(".nav-partners").textContent =
-    translations[lang].nav.partners;
+  document.querySelector(".nav-company").textContent = translations[lang].nav.company;
+  document.querySelector(".nav-training").textContent = translations[lang].nav.training;
+  document.querySelector(".nav-videos").textContent = translations[lang].nav.videos;
+  document.querySelector(".nav-events").textContent = translations[lang].nav.events;
+  document.querySelector(".nav-partners").textContent = translations[lang].nav.partners;
 
+  // Main
   document.querySelector(".HomeText").textContent = translations[lang].title;
   document.querySelector(".subtitle").textContent = translations[lang].subtitle;
   document.querySelector(".botaoBotao").textContent = translations[lang].button;
 
-  document.querySelector(
-    ".footer-content p:first-child"
-  ).innerHTML = `&copy; 2025 QualyAgro - ${translations[lang].footer1}`;
+  // Footer
+  document.querySelector(".footer-content p:first-child").innerHTML =
+    `&copy; 2025 QualyAgro - ${translations[lang].footer1}`;
   document.querySelector(".footer-content p:nth-child(2)").textContent =
     translations[lang].footer2;
 
